eab1628c6dc847259ea0032acf14ab45f3d29727
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-16 21:12:28 -0600

    Able to encrypt and decrypt text with single letter key

--- main.py
+++ main.py
@@ -18,8 +18,10 @@ def readFile():
 # end method
 
 # function that encrypts a given text with key
-def ciphertext(str_list):
+def ciphertext(str_list,key):
     new_str_list = []
+
+    shiftSize = ord(key) - ord('a')
 
     # for-loop iterates through each element
     for x in str_list:
@@ -28,9 +30,10 @@ def ciphertext(str_list):
 
         # for-loop iterates through each char of element
         for c in char_list:
+
             # check if char is letter or number
             if c.isalpha() or c.isnumeric():
-                c = chr(ord(c) + 1)       
+                c = chr(ord(c) + shiftSize)       
             newline += c
 
         new_str_list.append(newline)    # add encypted line new_str_list
@@ -39,9 +42,10 @@ def ciphertext(str_list):
 # end method
 
 # method that deciphers a given text with key
-def decipher(str_list):
+def decipher(str_list,key):
     new_str_list = []
 
+    shiftSize = ord(key) - ord('a')
     # for-loop iterates through all lines
     for x in str_list:
         newline = ""
@@ -51,7 +55,7 @@ def decipher(str_list):
         for c in char_list:
             # check if char is a letter or number
             if c.isalpha() or c.isnumeric():
-                c = chr(ord(c) - 1)
+                c = chr(ord(c) - shiftSize)
 
             newline += c
         
@@ -63,9 +67,10 @@ def decipher(str_list):
 # start program
 str_list = readFile()
 
-cipher_list = ciphertext(str_list)
+key = 'c'
+cipher_list = ciphertext(str_list,key)
 print(cipher_list)
 
-plain_text = decipher(cipher_list)
+plain_text = decipher(cipher_list,key)
 print(plain_text)
 # end program
