89c44b2d0b22b227ee1371368c019adb5167c557
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-21 13:43:30 -0600

    Update: Cleaned up code logic when calculating new char for encryption and decryption

--- main.py
+++ main.py
@@ -70,18 +70,14 @@ def newChar(char,jumps):
 
     if char.isupper():
         if newCharInt > 90:
-            newCharInt -= 90
-            newCharInt += 64
+            newCharInt -= 26;
         elif newCharInt < 65:
-            newCharInt = 64 - newCharInt
-            newCharInt = 90 - newCharInt
+            newCharInt = 90 - (64 % newCharInt)
     else:
         if newCharInt > 122:
-            newCharInt -= 122
-            newCharInt += 96
+            newCharInt -= 26
         elif newCharInt < 97:
-            newCharInt = 96 - newCharInt
-            newCharInt = 122 - newCharInt
+            newCharInt = 122 - (96 % newCharInt)
 
     return chr(newCharInt)
 # end method
