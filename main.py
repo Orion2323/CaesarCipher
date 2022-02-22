5a1317d3c736b3ff61dbf6b98da2679ce14ac715
Giancarlos Dominguez <Giancarlosd@smu.edu>
2022-02-21 21:43:51 -0600

    Update: Recycled code for encryption/decryption of text

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def readFile():
     input_file.close()
 
     return str_list
-# end method
+# end function
 
-# function that encrypts a given text with key
-def ciphertext(str_list,key):
+# function that encrypt and decrypts text
+def encrypt_decrypt(str_list,key,mode):
     new_str_list = []
     keyCounter = 0
 
@@ -37,7 +37,11 @@ def ciphertext(str_list,key):
                 if (keyCounter >= len(key) - 1):
                     keyCounter = 0;
 
-            shiftSize = ord(key[keyCounter]) - ord('a')      # get difference from key letter and 'a' decimal values
+            # calculate jumps according to whether function encrypts or decrypts
+            if (mode == 'e'):
+                shiftSize = ord(key[keyCounter]) - ord('a')    # get difference from key letter and 'a' decimal values
+            elif (mode == 'd'):
+                shiftSize = ord('a') - ord(key[keyCounter])    # get difference from key letter and 'a' decimal values
 
             # check if char is letter or number
             if c.isalpha() or c.isnumeric():
@@ -53,45 +57,7 @@ def ciphertext(str_list,key):
         new_str_list.append(newline)    # add encypted line new_str_list
     
     return new_str_list
-# end method
-
-# method that deciphers a given text with key
-def decipher(str_list,key):
-    new_str_list = []
-    keyCounter = 0
-
-    # for-loop iterates through all lines
-    for x in str_list:
-        newline = ""
-        char_list = list(x)
-
-        # for-loop iterates through all chars in line
-        for c in char_list:
-            # check if key char is a letter
-            while not (key[keyCounter].isalpha()):
-                keyCounter += 1
-
-                # check if reached the last char of key
-                if (keyCounter >= len(key) - 1):
-                    keyCounter = 0;
-
-            shiftSize = ord('a') - ord(key[keyCounter])    # get difference from key letter and 'a' decimal values
-
-            # check if char is a letter or number
-            if c.isalpha() or c.isnumeric():
-                c = newChar(c,shiftSize)
-
-            newline += c
-            keyCounter += 1
-
-            # check if reached the last char of key
-            if (keyCounter >= len(key) - 1):
-                keyCounter = 0;
-
-        new_str_list.append(newline)    # add deciphered line to new_str_list
-    
-    return new_str_list
-# end method
+# end function
 
 # function that calculates ASCII code of new 
 def newChar(char,jumps):
@@ -109,13 +75,13 @@ def newChar(char,jumps):
             newCharInt = 122 - (96 % newCharInt)
 
     return chr(newCharInt)
-# end method
+# end function
 
 # start program
 correctInput = False
 # check if user typed in correct options
 while not (correctInput):
-    fileChoice = input("Do you want to read file? (y/n)\n").lower()
+    fileChoice = input("Do you want to read file? (y/n): ").lower()
     
     # check if user input is a 'y' or 'n'
     if (fileChoice == 'y' or fileChoice == 'n'):
@@ -128,23 +94,23 @@ str_list = []
 # check if user wants to use input file or input custom plaintext
 if fileChoice.lower() == 'y':
     str_list = readFile()
-    key = input("Enter a key: ")
+    key = input("Enter key: ")
 elif fileChoice.lower() == 'n':
     text = input("Enter text to encrypt: ")
     str_list.append(text)
-    key = input("Enter single letter key: ")
+    key = input("Enter key: ")
 else:
     print("Invalid input!")
 
 # encrypt plaintext
-cipher_list = ciphertext(str_list,key)
+cipher_list = encrypt_decrypt(str_list,key,'e')
 print("Encrypted text:")
 print(*cipher_list, sep = '\n')
 
 print()
 
 #decrypt ciphertext
-plain_text = decipher(cipher_list,key)
+plain_text = encrypt_decrypt(cipher_list,key,'d')
 print("Decrypted text:")
 print(*plain_text,sep = '\n')
 # end program
